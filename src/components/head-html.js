ef44f00cdb2c2c5db7bb6838acbc239b42dde9c8
gglas <gordonglas@gmail.com>
2023-04-26 19:20:45 -0500

    remove tags from head component that are already inserted at build time

--- src/components/head-html.js
+++ src/components/head-html.js
@@ -1,13 +1,11 @@
 import * as React from "react"
 
+// This html gets injected at runtime.
+// Use src/html.js if you need to put html at build-time.
 const HeadHtml = ({pageTitle}) => {
   return (
     <>
       <title>{pageTitle}</title>
-      {/* Files in the /static folder get copied 'as-is' to the final site's root */}
-      <link rel="apple-touch-icon" sizes="96x96" href="/img/gn/touch-icon-96-96.png" />
-      {/* For Mastodon link verification: https://docs.joinmastodon.org/user/profile/#verification */}
-      <link href="https://mastodon.gamedev.place/@kefka" rel="me" />
     </>
   )
 }
--- src/html.js
+++ src/html.js
@@ -14,7 +14,9 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        {/* Files in the /static folder get copied 'as-is' to the final site's root */}
         <link rel="apple-touch-icon" sizes="96x96" href="/img/gn/touch-icon-96-96.png" />
+        {/* For Mastodon link verification: https://docs.joinmastodon.org/user/profile/#verification */}
         <link href="https://mastodon.gamedev.place/@kefka" rel="me" />
         {props.headComponents}
       </head>
